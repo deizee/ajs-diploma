150d1527cca2ab7960f59e6c5babce4161a50538
Deizee <deizee@mail.ru>
2021-03-15 20:14:11 +0500

    отрисовка команд

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,12 +1,41 @@
+import themes from './themes';
+import Team from './Team';
+import { generateTeam } from './generators';
+import PositionedCharacter from './PositionedCharacter';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.userTeam = generateTeam(new Team().userTeam, 1, 2);
+    this.computerTeam = generateTeam(new Team().computerTeam, 1, 2);
   }
 
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+    this.gamePlay.drawUi(themes.prairie);
+
+    const userCoordinates = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
+    const compCoordinates = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+
+    const userTeamWithPositions = this.userTeam.map(
+      (el) =>
+        new PositionedCharacter(
+          el,
+          userCoordinates[Math.floor(Math.random() * (userCoordinates.length - 1))]
+        )
+    );
+    const computerTeamWithPositions = this.computerTeam.map(
+      (el) =>
+        new PositionedCharacter(
+          el,
+          compCoordinates[Math.floor(Math.random() * (compCoordinates.length - 1))]
+        )
+    );
+
+    const positions = userTeamWithPositions.concat(computerTeamWithPositions);
+    this.gamePlay.redrawPositions(positions); // positions - массив из объектов PositionedCharacter
   }
 
   onCellClick(index) {
--- src/js/Team.js
+++ src/js/Team.js
@@ -1,3 +1,16 @@
-export default class Team {
+import Bowman from './characters/Bowman';
+import Swordsman from './characters/Swordsman';
+import Daemon from './characters/Daemon';
+// import Magician from './characters/Magician';
+import Undead from './characters/Undead';
+import Vampire from './characters/Vampire';
 
+const userTeam = [Bowman, Swordsman];
+const computerTeam = [Daemon, Undead, Vampire];
+
+export default class Team {
+  constructor() {
+    this.userTeam = userTeam;
+    this.computerTeam = computerTeam;
+  }
 }
